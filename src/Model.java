9f9c61f0e7bfe28ec50925f021b1a0bccb6ec7a4
Kevin Hoogendijk <kevinhoog@gmail.com>
2015-07-24 14:15:52 +0200

    Implements listener support in model

--- src/Model.java
+++ src/Model.java
@@ -1,8 +1,10 @@
 public class Model {
   private final Map<String, long> wordList;
+  private final Set<OnModelDataChangedListener> listeners;
 
   public Model() {
     wordList = new HashMap<>();
+    listeners = new HashSet<>();
   }
 
   public void addWord(String word) {
@@ -34,4 +36,34 @@ public class Model {
       }
     }
   }
+
+  public void addListener(OnModelDataChangedListener listener) {
+    if (listener != null) {
+      listeners.add(listener);
+    } else {
+      throw new IllegalArgumentException("Listener can't be null");
+    }
+  }
+
+  public void removeListener(OnModelDataChangedListener listener) {
+    if (listeners.contains(listener)) {
+      listeners.remove(listener);
+    } else {
+      throw new IllegalArgumentException("Listener is already removed");
+    }
+  }
+
+  public isListenerAdded(OnModelDataChangedListener listener) {
+    return listeners.contains(listener);
+  }
+
+  private void notifyListeners(String word, long newCount, long oldCount) {
+    for (OnModelDataChangedListener l : listeners) {
+      l.onModelDataChanged(word, newCount, oldCount);
+    }
+  }
+
+  public interface OnModelDataChangedListener {
+    void onModelDataChanged(String word, long newCount, long oldCount);
+  } 
 }
