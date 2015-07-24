61840eb64c49a75a00135d1b9cfe1acf443e2e8a
Kevin Hoogendijk <kevinhoog@gmail.com>
2015-07-24 15:40:11 +0200

    Added randomdelay function

--- src/Twiddler.java
+++ src/Twiddler.java
@@ -1,11 +1,41 @@
 import org.jibble.pircbot.PircBot;
 
+import java.util.Random;
+
 /**
  * Created by kritt on 7/24/15.
  */
 public class Twiddler extends PircBot{
-    public Twiddler(){
-        this.setName("Twiddler");
+    private Model model;
+    private Random rand;
+    public Twiddler(Model model){
+        this.setName("GenericRussian3");
+        this.model = model;
+        this.rand = new Random();
+    }
 
+    @Override
+    protected void onMessage(String channel, String sender, String login, String hostname, String message) {
+
+        if(message.equals("bark")){
+            randomDelay();
+            sendMessage(channel, "Hi " + sender + "!");
+        }
+
+        model.addSentence(message);
+        /*
+        for(String string: model.getWordList().keySet()){
+            System.out.println("#######################################################################");
+            System.out.println(string + " : " + model.getCountForWord(string));
+            System.out.println("#######################################################################");
+        }*/
+    }
+
+    public void randomDelay(){
+        try{
+            Thread.sleep((long)(rand.nextDouble()*5000));
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
